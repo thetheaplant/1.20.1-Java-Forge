7ef582b499589879bbcbc84ab4bd26545806e1e2
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-10-05 22:25:15 -0500

    Initial commit

---
+++ src/main/kotlin/io/github/techtastic/vs_addon_template/VSAddonTemplateMod.kt
@@ -0,0 +1,14 @@
+package io.github.techtastic.vs_addon_template
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(VSAddonTemplateMod.MOD_ID)
+class VSAddonTemplateMod {
+    init {
+        // Common Code Here
+    }
+
+    companion object {
+        const val MOD_ID = "vs_addon_template"
+    }
+}
