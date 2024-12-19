5e9a6bbb40f5ee4330cf0254ca2a23132bef6e43
likzerd <122873842+likzerd@users.noreply.github.com>
2024-12-19 21:10:16 +0800

    usual property changes

---
+++ src/main/java/io/github/thetheaplant/vs_nyoom/VSNyoomMod.java
@@ -0,0 +1,12 @@
+package io.github.thetheaplant.vs_nyoom;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VSNyoomMod.MOD_ID)
+public class VSNyoomMod {
+    public static final String MOD_ID = "vs_nyoom";
+
+    public VSNyoomMod() {
+
+    }
+}
