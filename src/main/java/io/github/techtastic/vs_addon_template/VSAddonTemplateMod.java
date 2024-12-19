a5172e8f3db8cd2b2afee5fa0f40c09d64510d7b
likzerd <122873842+likzerd@users.noreply.github.com>
2024-12-19 20:07:58 +0800

    test commit

--- src/main/java/io/github/techtastic/vs_addon_template/VSAddonTemplateMod.java
+++ src/main/java/io/github/techtastic/vs_addon_template/VSAddonTemplateMod.java
@@ -7,6 +7,6 @@ public class VSAddonTemplateMod {
     public static final String MOD_ID = "vs_addon_template";
 
     public VSAddonTemplateMod() {
-        // Common Code Here
+        // Common Code Her
     }
 }
